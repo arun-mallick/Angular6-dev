0870d0bb591d3c006640a3fb0bc77a2957fe6295
apple <apple@apples-MacBook-Pro.local>
2018-08-27 20:48:16 +0530

    Second Assignment

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'data-binding',
+    templateUrl:'./data-binding.component.html'
+})
+
+export class DataBindingComponent {
+    userName = "";
+    name = "";
+    displayName(){
+        this.name = this.userName;
+    }
+    emptyString(){
+        this.userName = "";
+        this.name = "";
+    }
+}
